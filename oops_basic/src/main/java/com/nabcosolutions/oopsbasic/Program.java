ce639a7e564c0fdb1d90cfceeee1103fbbafa61d
Navin Bahadur Rana <nabinrana102@gmail.com>
2022-02-02 18:40:47 +0545

    first commit

---
+++ oops_basic/src/main/java/com/nabcosolutions/oopsbasic/Program.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nabcosolutions.oopsbasic;
+
+/**
+ *
+ * @author Navin
+ */
+public class Program {
+    
+    public static void main(String[] args){
+//        Prints Hello world 
+        System.out.println("Hello world");
+    }
+}
